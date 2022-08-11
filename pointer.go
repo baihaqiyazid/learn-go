ae17fd35898713de1a6bcfa080f6bd62abc9b70c
baihaqiyazid <baihaqiyazid16@gmail.com>
2022-08-11 15:36:01 +0700

    pointer in function

--- pointer.go
+++ pointer.go
@@ -5,23 +5,35 @@ import "fmt"
 type Address struct{
 	City, Province, Country string
 }
+func ChangeCountry(address *Address)  {
+	address.Country = "Indonesia"
+}
 
 func main() {
 	var address1 Address = Address{"Subang", "Jawa Barat", "Indonesia"}
 	var address2 *Address = &address1
-	var address3 *Address = &address1
+	// var address3 *Address = &address1
 
 	address2.City = "Bandung"
 
 	*address2 = Address{"Malang", "Jawa TImur", "Indonesia"}
 	
-	fmt.Println(address1)
-	fmt.Println(address2)
-	fmt.Println(address3)
+	// fmt.Println(address1)
+	// fmt.Println(address2)
+	// fmt.Println(address3)
 
 	var address4 *Address = new(Address)
 	address4.City = "Surabaya"
 	address4.Province = "Jawa Timur"
 	address4.Country = "Indonesia"
-	fmt.Println(address4)
+	// fmt.Println(address4)
+
+	address5 := Address {
+		City: "Papua",
+		Province: "Maluku",
+		Country: "",
+	}
+
+	ChangeCountry(&address5)
+	fmt.Println(address5)
 }
